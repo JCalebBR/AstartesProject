cf15f3f4a68e494ca5a36754adc947a992492af6
Josué Caleb <josshuacaleb@gmail.com>
2024-07-02 18:09:18 -0300

    letsgo

--- Mod.cs
+++ Mod.cs
@@ -24,12 +24,6 @@ namespace AstartesProject
         public static AssetBundle bundle;
         public static KitchenLogger Logger;
 
-        public static List<int> OutfitIDS = new List<int>();
-        public static List<int> HatIDS = new List<int>();
-
-        public static List<string> OutfitNames = new List<string>();
-        public static List<string> HatNames = new List<string>();
-
         public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
 
         protected override void OnInitialise()
@@ -46,29 +40,7 @@ namespace AstartesProject
             bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).FirstOrDefault() ?? throw new MissingAssetBundleException(MOD_GUID);
             Logger = InitLogger();
 
-            RegisterHat<Mk4_Helmet>("Mk.4 Helmet");
-
-            ModsPreferencesMenu<PauseMenuAction>.RegisterMenu("Astartes Project", typeof(OutfitSelectionMenu<PauseMenuAction>), typeof(PauseMenuAction));
-
-            Events.PreferenceMenu_PauseMenu_CreateSubmenusEvent += (s, args) =>
-            {
-                args.Menus.Add(typeof(OutfitSelectionMenu<PauseMenuAction>), new OutfitSelectionMenu<PauseMenuAction>(args.Container, args.Module_list));
-            };
-        }
-
-        public void RegisterOutfit<T>(string displayName) where T : CustomPlayerCosmetic, new()
-        {
-            CustomPlayerCosmetic outfit = AddGameDataObject<T>();
-            OutfitIDS.Add(outfit.ID);
-            OutfitNames.Add(displayName);
-        }
-
-        public void RegisterHat<T>(string displayName) where T : CustomPlayerCosmetic, new()
-        {
-            CustomPlayerCosmetic outfit = AddGameDataObject<T>();
-            HatIDS.Add(outfit.ID);
-            HatNames.Add(displayName);
+            AddGameDataObject<Mk4_Helmet>();
         }
     }
 }
-
